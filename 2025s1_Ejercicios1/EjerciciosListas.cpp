4b5fe0cee3fc1c03226516f3da4d38abffcbde45
luisgagnevin@gmail.com <82191316+LuisGagn@users.noreply.github.com>
2025-04-15 21:01:19 -0300

    Listas

--- 2025s1_Ejercicios1/EjerciciosListas.cpp
+++ 2025s1_Ejercicios1/EjerciciosListas.cpp
@@ -1,10 +1,44 @@
 #include "EjerciciosListas.h"
 
+
+
+void agregarPrincipio(NodoLista* &lista, int dato) {
+
+	NodoLista* nodo = new NodoLista;
+	nodo->dato = dato;
+	nodo->sig = lista;
+	lista = nodo;
+}
+
+int largoLista(NodoLista* &lista) {
+	int largo = 0;
+	NodoLista* temp = lista;
+	while (temp) {
+		largo++;
+		temp = temp->sig;
+	}
+	return largo;
+	delete[] temp;
+}
+
 NodoLista* invertirParcial(NodoLista* l) 
 {
-	// IMPLEMENTAR SOLUCION
-	return NULL;
-}
+	// Inicializa y caso Nulo o 1 solo
+	if (!l || !l->sig) return nullptr;
+	NodoLista* listaInvertida = nullptr;
+
+	// Total de elementos
+	int longitud = largoLista(l);
+	NodoLista* temp = l;
+
+	// Itera hasta el penultimo y los agrega de a uno al comienzo de la nueva lista.
+	for (int i = 0; i < longitud - 1; ++i) {
+		agregarPrincipio(listaInvertida, temp->dato);
+		temp = temp->sig;
+	}
+		return listaInvertida;
+	}
+
 
 void eliminarNesimoDesdeElFinal(NodoLista*& lista, int &n) 
 {
