05bee72ee0bf3f6d112e468d34adde4a27d4d46f
LuisGagn <luisgagnevin@gmail.com>
2025-04-22 21:20:39 -0300

    sofi casa

--- 2025s1_Ejercicios1/EjerciciosArboles.cpp
+++ 2025s1_Ejercicios1/EjerciciosArboles.cpp
@@ -2,18 +2,57 @@
 
 
 int altura(NodoAB* raiz){
-	// IMPLEMENTAR SOLUCION
-	return 0;
+	if (raiz == nullptr) return 0;
+
+	int left = altura(raiz->izq);
+	int right = altura(raiz->der);
+
+	return max(left, right)+1;
 }
 
 bool sonIguales(NodoAB* p, NodoAB* q) {
-	// IMPLEMENTAR SOLUCION
-	return false;
+	if (p == nullptr && q == nullptr) return true;
+	if (p != nullptr && q == nullptr || p == nullptr && q != nullptr) return false;
+	if (p->dato == q->dato) {
+		bool izq = sonIguales(p->izq, q->izq);
+		bool der = sonIguales(p->der, q->der);
+		return izq&&der;
+	}
+	else {
+		return false;
+	}
+	
+	return true;
+}
+
+
+bool auxiliarCaminoSuma(NodoAB* raiz, int sum) {
+	if (raiz == NULL) {
+		return false;
+	}
+
+	// Si es hoja, verificar si la suma coincide
+	if (raiz->izq == nullptr && raiz->der == nullptr) {
+		return sum == raiz->dato;
+	}
+
+	// Restar el valor del nodo actual y verificar recursivamente en hijos
+	int sumaRestante = sum - raiz->dato;
+
+	return auxiliarCaminoSuma(raiz->izq, sumaRestante) ||
+		auxiliarCaminoSuma(raiz->der, sumaRestante);
 }
 
 bool existeCaminoConSuma(NodoAB* raiz, int sum) {
-	// IMPLEMENTAR SOLUCION
-	return false;
+
+
+	if (raiz == NULL) {
+		return sum == 0;
+	}
+
+	else {
+		return auxiliarCaminoSuma(raiz, sum);
+	}
 }
 
 bool esArbolBalanceado(NodoAB *raiz) {
