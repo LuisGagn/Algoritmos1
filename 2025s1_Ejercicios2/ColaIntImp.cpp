8253b41cdde52fa18314a00c1fd40a34f31d4073
Luis Gagñevin <82191316+LuisGagn@users.noreply.github.com>
2025-05-27 16:08:58 -0300

    Update ColaIntImp.cpp

--- 2025s1_Ejercicios2/ColaIntImp.cpp
+++ 2025s1_Ejercicios2/ColaIntImp.cpp
@@ -4,45 +4,77 @@
 
 //Si necestita otra estructura se puede definir aqui
 
+struct NodoCola{
+	int valor;
+	NodoCola* sig;
+};
+
 struct _representacionColaInt {
-	// NO IMPLEMENTADO
+	NodoCola* primero;
+	NodoCola* ultimo;
+	int cantElementos;
 };
 
 ColaInt crearColaInt() {
-	// NO IMPLEMENTADO
-	return NULL;
+	ColaInt c = new _representacionColaInt;
+	c->primero=c->ultimo=nullptr;
+	c->cantElementos = 0;
+	return c;
 }
 
 void encolar(ColaInt& c, int e) {
-	// NO IMPLEMENTADO
+	NodoCola* nuevo = new NodoCola;
+	nuevo->valor = e;
+	nuevo->sig = nullptr;
+	if(!c->primero){ 
+		c->primero = nuevo;
+	} else {
+		c->ultimo->sig = nuevo;
+	}
+	c->ultimo = nuevo;
+	c->cantElementos++;
 }
 
 int principio(ColaInt c) {
-	// NO IMPLEMENTADO
-	return 0;
+	return c->primero->valor;
 }
 
 void desencolar(ColaInt& c) {
-	// NO IMPLEMENTADO
+	NodoCola* temp = c->primero;
+	c->primero = c->primero->sig;
+	if(!c->primero) c->ultimo = nullptr;
+	delete temp;
+	c->cantElementos--;
 }
 
 bool esVacia(ColaInt c) {
-	// NO IMPLEMENTADO
-	return true;
+	return c->cantElementos==0;
 }
 
 unsigned int cantidadElementos(ColaInt c) {
-	// NO IMPLEMENTADO
-	return 0;
+	return c->cantElementos;
 }
 
 ColaInt clon(ColaInt c) {
-	// NO IMPLEMENTADO
-	return NULL;
+	ColaInt nueva = crearColaInt();
+	NodoCola* temp = c->primero;
+	while(temp){
+		encolar(nueva,temp->valor);
+		temp=temp->sig;
+	}
+
+	return nueva;
 }
 
 void destruir(ColaInt& c) {
-	// NO IMPLEMENTADO
+	NodoCola* temp;
+	while(c->primero){
+		temp=c->primero;
+		c->primero = c->primero->sig;
+		delete temp;
+	}
+	delete c;
+	c=nullptr;
 }
 
 #endif
