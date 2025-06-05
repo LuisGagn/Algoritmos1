007af68fdb6c403cfcf5c6a6dc385d18230fac7a
Luis Gagñevin <82191316+LuisGagn@users.noreply.github.com>
2025-06-05 15:00:52 -0300

    Update TablaIntStringImp.cpp

--- 2025s1_Ejercicios2/TablaIntStringImp.cpp
+++ 2025s1_Ejercicios2/TablaIntStringImp.cpp
@@ -4,18 +4,47 @@
 
 //Si necestita otra estructura se puede definir aqui
 
-struct _representacionTablaIntString {
-	// NO IMPLEMENTADO
+struct NodoHash {
+	int dom;
+	char* rn;
+	NodoHash* sig;
 };
 
+struct _representacionTablaIntString {
+	NodoHash** tabla;
+	unsigned int cantidad;
+	unsigned int cota;
+};
+
+
+int hashing(int i; int cota){
+	return abs(i%cota);
+}
+
 TablaIntString crearTablaIntString(unsigned int esperados) {
-	// NO IMPLEMENTADO
+	TablaIntString nueva = new _representacionTablaIntString;
+	nueva->cantidad = 0;
+	nueva->cota = esperados;
 	return NULL;
 }
 
 
 void agregar(TablaIntString& t, int d, const char* r) {
-	// NO IMPLEMENTADO
+	int celda = hashing(d, t->cota);
+	NodoHash* lista = t->tabla[celda];
+	while(lista && lista->dom!=d){
+		lista=lista->sig;
+	}
+	if(!lista){
+		NodoHash* nuevo = new NodoHash;
+		nuevo->dom = d;
+		nuevo->rn = r;
+		nuevo->sig = t->tabla[celda];
+		t->tabla[celda]=nuevo;
+		t->cantidad++;
+	} else {
+		lista->rn =r;
+	}
 }
 
 bool estaDefinida(TablaIntString t, int d) {
@@ -39,12 +68,12 @@ int elemento(TablaIntString t) {
 
 bool esVacia(TablaIntString t) {
 	// NO IMPLEMENTADO
-	return true;
+	return t->cantidad==0;
 }
 
 unsigned int cantidadElementos(TablaIntString t) {
 	// NO IMPLEMENTADO
-	return 0;
+	return t->cantidad;;
 }
 
 void destruir(TablaIntString& t) {
